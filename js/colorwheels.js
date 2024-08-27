76d6728faa09a548d5bd951ec17ea1ce2f20ed05
Yoichi Imai <sunnyone41@gmail.com>
2024-08-28 01:00:01 +0900

    Remove an alert

--- js/colorwheels.js
+++ js/colorwheels.js
@@ -96,8 +96,6 @@ function renderCircle(toneName, toneTitle, toneData, gCircle) {
          var parent = d3.select(this);
          var colorValue = parent.select(".color-value").text();
          navigator.clipboard.writeText(colorValue);
-
-         alert(colorValue + " をコピーしました！");
       });
     
     g.append("path")
